22bce61200e689e2826b2209dd80327785f15702
ZenithEmpero <neilzeith@gmail.com>
2023-05-17 12:25:22 +0800

    Player Class

--- library.py
+++ library.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import time as t
 import random as r
+import math as m
 import sys
 from settings import *
 from threading import Thread
@@ -9,25 +10,30 @@ class Body:
     def __init__(self, main):
         self.window = main.window
         self.speed = obstacle_speed
+        self.main = main
+        self.player = Player(self)
+        self.water = Water(self)
 
         y1 = r.randint(200, 500)
         y2 = r.randint(200, 500)
 
 
-        self.obstacle1 = Pillar(main, self, (1300, y1))
-        self.obstacle2 = Pillar(main, self, (2050, y2))
+        self.obstacle1 = Pillar(self, (1300, y1))
+        self.obstacle2 = Pillar(self, (2050, y2))
 
 
     def draw(self):
         self.obstacle1.draw()
         self.obstacle2.draw()
+        self.water.draw()
+        self.player.draw()
 
 
 
 class Pillar:
-    def __init__(self, main, body, pos):
-        self.window = main.window
-        self.main = main
+    def __init__(self, body, pos):
+        self.main = body.main
+        self.window = self.main.window
         self.body = body
         self.coordinate = pos
         self.gap = 600
@@ -55,3 +61,38 @@ class Pillar:
                 pg.time.wait(5000)
                 self.body.speed += 0.01
 
+class Player:
+    def __init__(self, body):
+        self.player_pos = (200, 300)
+        self.body = body
+        self.main = body.main
+        
+
+    def draw(self):
+        pg.draw.circle(self.main.window, 'red', self.player_pos, 25)
+
+    def controls(self):
+        pass
+
+class Water:
+    def __init__(self, body):
+        self.body = body
+        self.main = body.main
+        self.water_pos = (0, 400)
+
+        self.x = 0
+        self.y = 500
+
+    def draw(self):
+        self.draw_water_rect()
+
+    def draw_water_rect(self):
+        self.sin_wave()
+        self.water_rect = pg.Rect(self.water_pos[0], self.water_pos[1], 1200, self.y)
+        pg.draw.rect(self.main.window, 'blue', self.water_rect)
+
+
+    def sin_wave(self):
+        self.y = self.body.player.player_pos[1]
+        self.x += 1 * self.main.delta_time
+        self.y += m.sin(self.x) * self.main.delta_time
