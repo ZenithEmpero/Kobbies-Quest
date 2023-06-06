78232b906078ff2c3939b5b4cf436aeaf9063cf0
ZenithEmpero <neilzeith@gmail.com>
2023-06-06 19:20:20 +0800

    Menu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame as pg
 import sys
 from settings import *
 from library import *
-
+from menu import *
 
 class Main:
     def __init__(self):
@@ -10,20 +10,21 @@ class Main:
         self.window = pg.display.set_mode((WIDTH, HEIGHT))
         self.body = Body(self)
         self.clock = pg.time.Clock()
+        self.menu = Menu(self)
 
         self.score = 0
-        
-
+    
 
     def update(self):
-        self.delta_time = self.clock.tick(FPS)
-        pg.display.set_caption(f'FPS: [{round(self.clock.get_fps(), 1)}],       SCORE: [{self.score}]')
-        self.draw()
-        pg.display.flip()
+        if not self.menu.running:
+            self.delta_time = self.clock.tick(FPS)
+            pg.display.set_caption(f'FPS: [{round(self.clock.get_fps(), 1)}],       SCORE: [{self.score}]')
+            self.draw()
+            pg.display.flip()
 
     def draw(self):
-        self.window.fill('black')
-        self.window.blit(sky_background, (0, 0))
+        self.window.fill('sky blue')
+        #self.window.blit(sky_background, (0, 0))
         self.body.draw()
 
 
@@ -37,7 +38,10 @@ class Main:
     def run(self):
         while self.running:
             self.check_events()
-            self.update()
+            if not self.menu.running:
+                self.update()
+            else:
+                self.menu.update()
 
 
 if __name__ == '__main__':
